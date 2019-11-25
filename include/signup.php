d4f9701a18660f518e7e45d5ef037d5ae5edc3de
scatterbrain3 <scattini3@hotmail.com>
2019-11-26 00:32:42 +1030

    added mysqli_real_escape_string

--- include/signup.php
+++ include/signup.php
@@ -3,11 +3,11 @@
 include_once "db.php";
 
 if(isset($_POST["submit"])){
-    $first = $_POST["first"];
-    $last = $_POST["last"];
-    $email = $_POST["email"];
-    $uid = $_POST["uid"];
-    $pwd = $_POST["pwd"];
+    $first = mysqli_real_escape_string($_POST["first"]);
+    $last = mysqli_real_escape_string($_POST["last"]);
+    $email = mysqli_real_escape_string($_POST["email"]);
+    $uid = mysqli_real_escape_string($_POST["uid"]);
+    $pwd = mysqli_real_escape_string($_POST["pwd"]);
 
     $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$pwd')";
     mysqli_query($conn, $sql);
