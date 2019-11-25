861ebbf617b4855ed0acf7b8c3840e72f6f663d7
scatterbrain3 <scattini3@hotmail.com>
2019-11-26 04:11:45 +1030

    added vallidation

--- include/signup.php
+++ include/signup.php
@@ -3,26 +3,41 @@
 include_once "db.php";
 
 if(isset($_POST["submit"])){
+
     $first = mysqli_real_escape_string($conn, $_POST["first"]);
     $last = mysqli_real_escape_string($conn, $_POST["last"]);
     $email = mysqli_real_escape_string($conn, $_POST["email"]);
     $uid = mysqli_real_escape_string($conn, $_POST["uid"]);
     $pwd = mysqli_real_escape_string($conn, $_POST["pwd"]);
 
-    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?)";
+   
+    if(empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)){
+        header("location: ../index.php?signup=empty&first=$first&last=$last&uid=$uid");
+        exit();
+    }
+    else {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            header("Location: ../index.php?signup=email&first=$first&last=$last&uid=$uid");
+            exit();
+        }
+        else{
+
+            $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?)";
     
-    $stmt = msqli_stmt_init($conn);
-    if(!mysqli_stmt_prepare($stmt, $sql)){
-        echo "Statment failed";
+            $stmt = mysqli_stmt_init($conn);
+            if(!mysqli_stmt_prepare($stmt, $sql)){
+                echo "Statment failed";
+            }
+            else{
+                mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+                mysqli_stmt_execute($stmt);
+            }
+        
+            header("Location: ../index.php?signup=success");        
+
+        }
     }
-    else{
-        mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
-        mysqli_stmt_execute($stmt);
-    }
-
-
-    header("Location: ../index.php?signup=success");
-
-
 }
-
+else{
+    header("Location: ../index.php?signup=error");
+}
--- index.php
+++ index.php
@@ -12,18 +12,53 @@
     <body>
 
         <form action="include/signup.php" method="POST">
-            <input type="text" name="first" placeholder="Firstname">
+            <input type="text" name="first" placeholder="Firstname" value="<?php echo isset($_GET["first"]) ? $_GET["first"]: "" ?>">
             <br />
-            <input type="text" name="last" placeholder="Lastname">
+            <input type="text" name="last" placeholder="Lastname" value="<?php echo isset($_GET["last"]) ? $_GET["last"]: "" ?>">
             <br />
-            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="email" placeholder="Email" value="<?php echo isset($_GET["email"]) ? $_GET["email"]: "" ?>">
             <br />
-            <input type="text" name="uid" placeholder="Username">
+            <input type="text" name="uid" placeholder="Username" value="<?php echo isset($_GET["uid"]) ? $_GET["uid"]: "" ?>">
             <br />
             <input type="password" name="pwd" placeholder="Password">
             <br />
             <button name="submit" type="submit" value="submit">Submit</button>
         </form>
+        <?php
+
+            /*$fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+            if(strpos($fullUrl, "signup=empty") == true){
+                echo "<p>Please fill in all fields</p>";
+            }
+            else if(strpos($fullUrl, "signup=email") == true){
+                echo "<p>Please enter a vallid email</p>";
+            }
+            else if(strpos($fullUrl, "signup=success") == true){
+                echo "<p>Signup success</p>";
+            }*/
+
+            if(!isset($_GET["signup"])){
+                exit();
+            }
+
+            else{
+                $signupCheck = $_GET["signup"];
+
+                if($signupCheck == "empty"){
+                    echo "<p>Please fill in all fields</p>";
+                    exit();
+                }            
+                else if($signupCheck == "email"){
+                    echo "<p>Please enter a vallid email</p>";
+                    exit();
+                } 
+                else if($signupCheck == "success"){
+                    echo "<p>Signup success</p>";
+                    exit();
+                }
+            }   
+        ?>
 
 
     </body>
